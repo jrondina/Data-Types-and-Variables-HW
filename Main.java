8b2324e59a3f3a4f7f67030d771c4446238c7aab
jrondina <jamesrondina@Gmail.com>
2016-06-21 23:41:48 -0400

    filling out blanks for HW

--- Main.java
+++ Main.java
@@ -20,18 +20,18 @@ public class Main {
          * Hint 2 - try running the code (right click on this file and select "Run Main.main()" and if you get
          * zero errors then your data types and values all match up
          */
-        ___ varA = 3;
-        ___ varB = 9.75;
-        ___ varC = 4.0;
-        ___ varD = 2.;
-        ___ varE = 8573f;
-        ___ varF = "Gelato is delicious!";
-        ___ varG = '$';
-        ___ varH = "$";
-        ___ varI = true;
-        ___ varJ = (2 == 3);
-        ___ varK = Integer.valueOf("9,235");
-        ___ varL = varF.equals("Gelato is delicious!");
+        int varA = 3;
+        double varB = 9.75;
+        double varC = 4.0;
+        double varD = 2.;
+        float varE = 8573f;
+        String varF = "Gelato is delicious!";
+        char varG = '$';
+        String varH = "$";
+        boolean varI = true;
+        boolean varJ = (2 == 3);
+        int varK = Integer.valueOf("9235");
+        boolean varL = varF.equals("Gelato is delicious!");
 
 
         /**
@@ -43,8 +43,8 @@ public class Main {
          * be done in separate lines of code.
          */
 
-        ___ price;
-        price = ___;
+        double price;
+        price = 3.50;
 
 
         /**
@@ -52,7 +52,7 @@ public class Main {
          * give it an initial value of "placeholder comment" all in one line of code.
          */
 
-        ___ comment = ___;
+        String comment = "placeholder comment";
 
 
         /**
@@ -66,7 +66,7 @@ public class Main {
 
         int numerator = 10;
         int divisor = 3;
-        int divisionResult = numerator / divisor;
+        int divisionResult = numerator / divisor; //3
         System.out.println(divisionResult);
 
     }
